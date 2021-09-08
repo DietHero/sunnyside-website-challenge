9cad6dfc8db2043fa379f3efa7a488f1c227161a
Karina Białek <karinabialek2@gmail.com>
2021-09-08 19:08:07 +0200

    js menu added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 'use strick'
 var button = document.querySelector(".button_menu")
 var navlinks = document.querySelector('.nav__links')
-var navlink = document.querySelector('.nav__link')
+const navlink = document.querySelectorAll('.nav__link-item')
+var contact = document.querySelector('.nav_contact')
+var arrow = document.querySelector('.arrow')
+var button = document.querySelector('.button_menu-icon')
+
+
 
 button.addEventListener ('click', () =>
 {
-    navlinks.classList.toggle("nav__links-oncick");
-    for (i=0; i < navlink.lenght; i++) {
-        navlink[i].classList.toggle('nav__link-click');
-    }
-
+    navlinks.classList.toggle("nav__links-click");
+    navlink.forEach(link => {
+        link.classList.toggle("nav__link-click");
+    contact.classList.toggle('nav_contact-click');
+    arrow.classList.toggle('arrow-click');
+    button.classList.toggle('button_menu-icon-click')
+    });
+    
 })
